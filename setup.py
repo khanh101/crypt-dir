73c4d15950aed9b7654c58e40040c9152380820e
khanh nguyen <khanh.nguyen.contact@gmail.com>
2023-01-27 11:25:05 +0800

    m

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     setuptools.setup(
         name="crypt-dir",
-        version="0.1.2",
+        version="0.1.3",
         author="Nguyen Ngoc Khanh",
         author_email="khanh.nguyen.contact@gmail.com",
         long_description=long_description,
